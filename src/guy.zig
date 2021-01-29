295d05178b07fc1b264db1a3a36d965d58ddfa6e
Darltrash <darlex259@gmail.com>
2021-01-29 00:28:46 -0300

    fixed typo lmao

--- src/guy.zig
+++ src/guy.zig
@@ -2,7 +2,7 @@ const T = 0x00000000;
 const B = 0x11000000;
 const W = 0xFFFFFFFF;
 const P = 0xFFad03fc;
-pub const pixels = [][32*32]u32 { // Tow he is safe here :)
+pub const pixels = [][32*32]u32 { // Now he is safe here :)
     P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,P,
     P,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,P,
     P,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T,P,
